7fca82f60c58281d9d8e57e65c57232c0afe0f3b
DylanC <d.charton@fimainfo.fr>
2023-07-15 12:07:29 +0200

    Imported axios and first request for the background

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "http://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=511754be0c611ae96791bbf5fac4e3fe";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/"
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL}
